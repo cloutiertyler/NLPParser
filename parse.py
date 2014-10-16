d5d4f785316a0df4c9c9a14b458464440793b341
Tyler F Cloutier <tclouti1@ugrad5.cs.jhu.edu>
2014-10-16 11:52:34 -0400

    Refactored the Parser class, made method with common code and made the parse function clearer.

--- parse.py
+++ parse.py
@@ -2,6 +2,20 @@ from copy import *
 from argparse import *
 from collections import defaultdict
 
+# The Grab class is necessary so that we can retrieve an element from a Python
+# Set in O(1) time. We could also have used our own implementation of a set
+# using a hash table.
+class Grab:
+    def __init__(self, value):
+        self.search_value = value
+    def __hash__(self):
+        return hash(self.search_value)
+    def __eq__(self, other):
+        if self.search_value == other:
+            self.actual_value = other
+            return True
+        return False
+
 class Parser(object):
     def __init__(self, grammar_file_name):
         self.parse_table = None
@@ -14,6 +28,39 @@ class Parser(object):
                     continue
                 groups = line.split()
                 self.rules[groups[1]].append(ProductionRule(groups[1], tuple(groups[2:]), float(groups[0])))
+   
+    def add_state_to_column(self, state, column, column_set):
+        if state not in column_set:
+            column.append(state)
+            column_set.add(state)
+        else:
+            grabber = Grab(state)             
+            grabber in column_set
+            if state.rule.weight < grabber.actual_value.rule.weight:
+                grabber.actual_value.rule.weight = state.rule.weight
+
+    def predict(self, predict_symbol, start_position, column, column_set):
+        predict_rules = self.rules[predict_symbol]
+        for predict_rule in predict_rules:
+            new_state = ParseState(start_position, predict_rule)
+            self.add_state_to_column(new_state, column, column_set)
+    
+    def scan(self, word, terminal, cur_state, next_column, next_column_set):
+        if word == terminal:
+            new_state = deepcopy(cur_state)
+            new_state.adv_state() #terminals are free
+            self.add_state_to_column(new_state, next_column, next_column_set)
+
+    def attach(self, state, column, column_set):
+        #for all incomplete states that end where this completed state starts
+        for previous_state in self.parse_table[state.start_pos]:
+            previous_symbols = previous_state.rule.symbols
+            
+            #This may be interesting if nonterinals are in the sentence.
+            if previous_symbols and previous_symbols[0] == state.rule.nonterminal:
+                new_state = deepcopy(previous_state)
+                new_state.adv_state(state.rule.weight)
+                self.add_state_to_column(new_state, column, column_set)
 
     def parse_sentence(self, sentence):
         self.parse_table = defaultdict(list)
@@ -30,46 +77,16 @@ class Parser(object):
                 state = column[j]
                 symbols = state.rule.symbols
                 if symbols:
-                    predict_symbol = symbols[0]
-                    if predict_symbol in self.rules:
+                    next_symbol = symbols[0]
+                    if next_symbol in self.rules:
                         #predict
-                        predict_rules = self.rules[predict_symbol]
-                        for predict_rule in predict_rules:
-                            new_state = ParseState(i, predict_rule)
-                            if new_state not in column_set:
-                                column.append(new_state)
-                                column_set.add(new_state)
+                        self.predict(next_symbol, i, column, column_set)
                     else:
                         #scan
-                        if word == predict_symbol:
-                            new_state = deepcopy(state)
-                            new_state.adv_state() #terminals are free
-                            if new_state not in next_column_set:
-                                next_column.append(new_state)
-                                next_column_set.add(new_state)
-                            else:
-                                grabber = Grab(new_state) #This is the one terrible thing about python
-                                grabber in next_column_set
-                                if new_state.rule.weight < grabber.actual_value.rule.weight:
-                                    grabber.actual_value.rule.weight = new_state.rule.weight
+                        self.scan(word, next_symbol, state, next_column, next_column_set)
                 else:
                     #complete
-                    #for all incomplete states that end where this completed state starts
-                    for previous_state in self.parse_table[state.start_pos]:
-                        previous_symbols = previous_state.rule.symbols
-                        
-                        #This may be interesting if nonterinals are in the sentence.
-                        if previous_symbols and previous_symbols[0] == state.rule.nonterminal:
-                            new_state = deepcopy(previous_state)
-                            new_state.adv_state(state.rule.weight)
-                            if new_state not in column_set:
-                                column.append(new_state)
-                                column_set.add(new_state)
-                            else:
-                                grabber = Grab(new_state) #This is the one terrible thing about python
-                                grabber in column_set
-                                if new_state.rule.weight < grabber.actual_value.rule.weight:
-                                    grabber.actual_value.rule.weight = new_state.rule.weight
+                    self.attach(state, column, column_set)
                                 
                 print 'column ' + str(i)
                 for entry in column:
@@ -115,17 +132,6 @@ class ProductionRule(object):
     
     def __repr__(self):
         return '%s -> .%s' % (self.nonterminal, ' '.join(self.symbols))
-
-class Grab:
-    def __init__(self, value):
-        self.search_value = value
-    def __hash__(self):
-        return hash(self.search_value)
-    def __eq__(self, other):
-        if self.search_value == other:
-            self.actual_value = other
-            return True
-        return False
 
 def main():
     parser = ArgumentParser()
